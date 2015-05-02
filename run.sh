92beaa852eee327632a00e7e6e55d3ad2d032008
Pete Birley <petebirley@gmail.com>
2015-05-02 21:36:36 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo date
 
-CONTAINER=$(sudo docker run -d -p 80:80 -p 443:443 --hostname="pxe.cannyos.local" -v /sys/fs/cgroup:/sys/fs/cgroup:ro maci/systemd /usr/lib/systemd/systemd)
+CONTAINER=$(sudo docker run -d -p 80:80 -p 443:443 --hostname="pxe.cannyos.local" maci/systemd /usr/lib/systemd/systemd)
 echo ${CONTAINER}
 echo "to enter docker container run: 
         docker exec -t -i ${CONTAINER} /bin/bash"
