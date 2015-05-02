c074d91a40e892f020f7b510c7805bd42b92db86
Pete Birley <petebirley@gmail.com>
2015-05-02 23:08:34 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 sudo date
 
-CONTAINER=$(sudo docker run -it -p 80:80 -p 443:443 -p 3000:3000 --hostname="pxe.cannyos.local" maci/systemd )
-docker kill ${CONTAINER}
-docker rm ${CONTAINER}
+docker run -it --rm -p 80:80 -p 443:443 -p 3000:3000 --hostname="pxe.cannyos.local" maci/systemd
 
 #RAILS_ENV=production bundle exec rake permissions:reset
 #./script/rails s -e production
