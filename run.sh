bbec620b527e8d317c9812918d9d15ce0a5f7ec1
Pete Birley <petebirley@gmail.com>
2015-05-02 22:43:58 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,6 @@ CONTAINER=$(sudo docker run -d -p 80:80 -p 443:443 -p 3000:3000 --hostname="pxe.
 echo ${CONTAINER}
 echo "to enter docker container run: 
         docker exec -t -i ${CONTAINER} /bin/bash"
-
+docker exec -t -i ${CONTAINER} /bin/bash
+docker kill ${CONTAINER}
+docker rm ${CONTAINER}
